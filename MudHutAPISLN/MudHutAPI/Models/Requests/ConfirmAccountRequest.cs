1f79304bdacb7d463e9245f951002b197695eb76
George <gtg_cerint@hotmail.com>
2022-07-25 10:48:06 +0100

    adsfoug

--- MudHutAPISLN/MudHutAPI/Models/Requests/ConfirmAccountRequest.cs
+++ MudHutAPISLN/MudHutAPI/Models/Requests/ConfirmAccountRequest.cs
@@ -4,5 +4,7 @@
     {
         public string userId { get; set; } = string.Empty;
         public string code { get; set; } = string.Empty;
+
+        public string code { get; set; } = string.Empty;
     }
 }
